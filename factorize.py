62f2b8be398afef7a1aa56c3ae45658312ed8db8
Merv Fansler <mmfansler@gmail.com>
2016-04-04 01:56:57 -0400

    factorize recursively working

--- factorize.py
+++ factorize.py
@@ -1,12 +1,21 @@
 
 
 def main():
+
+    print "DGC Toy"
+    for k, vs in Delta_g_DGC.items():
+        print k, " = ", factorize_recursive(vs)
+
+    print "\n\nBorromean Rings"
     for k, vs in Delta_g_BR.items():
         print k, " = ", factorize(vs)
 
+    print "\n\n3-Tuples Factoring"
+    print factorize_recursive(simple_triples)
+
 
 def factorize(ts):
-    sets = [(set([l]), set([r])) for (l, r) in ts]
+    sets = [(frozenset([l]), frozenset([r])) for (l, r) in ts]
 
     last_size = -1
     cur_size = len(sets)
@@ -21,12 +30,82 @@ def factorize(ts):
         for l, r in left_collect.items():
             right_collect[frozenset(r)].symmetric_difference_update(l)
 
-        sets = [(set(l), set(r)) for r, l in right_collect.items()]
+        sets = [(frozenset(l), frozenset(r)) for r, l in right_collect.items()]
 
         last_size = cur_size
         cur_size = len(sets)
 
     return sets
+
+
+def factorize3(ts):
+
+    sets = [(frozenset([l]), frozenset([(m, r)])) for (l, m, r) in ts]
+
+    last_size = -1
+    cur_size = len(sets)
+
+    while cur_size - last_size:
+
+        left_collect = {l: set([]) for (l, _) in sets}
+        for (l, r) in sets:
+            left_collect[l].symmetric_difference_update(r)
+        #print left_collect
+        left_collect = {l: factorize(list(r)) for l, r in left_collect.items()}
+        #print left_collect
+        right_collect = {frozenset(r): set([]) for r in left_collect.values()}
+        for l, r in left_collect.items():
+            right_collect[frozenset(r)].symmetric_difference_update(l)
+
+        sets = [(frozenset(l), r) for r, l in right_collect.items()]
+
+        last_size = cur_size
+        cur_size = len(sets)
+
+    # flatten everything out
+    return [(l, m, r) for (l, mr) in sets for (ms, rs) in mr for m in ms for r in rs]
+
+
+def factorize_recursive(tps):
+
+    if not isinstance(tps[0], tuple) or len(tps[0]) < 2:
+        return tps
+
+    sets = []
+    if len(tps[0]) == 2:
+        sets = [(frozenset([tp[0]]), frozenset([tp[1]])) for tp in tps]
+    else:
+        sets = [(frozenset([tp[0]]), frozenset([tp[1:]])) for tp in tps]
+
+    last_size = -1
+    cur_size = len(sets)
+
+    while cur_size - last_size:
+
+        left_collect = {l: set([]) for (l, _) in sets}
+        for (l, r) in sets:
+            left_collect[l].symmetric_difference_update(r)
+
+        left_collect = {l: factorize_recursive(list(r)) for l, r in left_collect.items()}
+
+        right_collect = {frozenset(r): set([]) for r in left_collect.values()}
+        for l, r in left_collect.items():
+            right_collect[frozenset(r)].symmetric_difference_update(l)
+
+        sets = [(frozenset(l), r) for r, l in right_collect.items()]
+
+        last_size = cur_size
+        cur_size = len(sets)
+
+
+    # flatten everything out
+    if len(tps[0]) == 2:
+        return sets
+    else:
+        return [(l,) + t for (l, r) in sets for t in r]
+
+
+Delta_g_DGC = {'h1_0': [('v', 'a'), ('a', 'v')], 'h0_0': [('v', 'v')], 'h2_0': [('v', 'ab'), ('a', 'b'), ('ab', 'v')]}
 
 
 Delta_g_BR = {'h0_0': [('v_{1}', 'v_{1}')],
@@ -46,5 +125,8 @@ Delta_g_BR = {'h0_0': [('v_{1}', 'v_{1}')],
               'h1_1': [('c_{7}', 'v_{3}'), ('v_{2}', 'c_{3}'), ('c_{3}', 'v_{3}'), ('v_{2}', 'c_{7}')],
               'h1_2': [('m_{9}', 'v_{7}'), ('m_{6}', 'v_{7}'), ('v_{6}', 'm_{9}'), ('v_{6}', 'm_{6}')]
               }
+
+simple_triples = [(1, 1, 1), (1, 1, 2), (1, 2, 1), (1, 2, 2), (2, 1, 1), (2, 1, 2), (2, 2, 1), (2, 2, 2),
+                  (1, 3, 1), (1, 3, 2), (2, 3, 1), (2, 3, 2)]
 
 main()
